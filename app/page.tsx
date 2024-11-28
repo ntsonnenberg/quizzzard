f53025174eebcf5d24585d8f6b0db62dd8bb103a
Nathan Sonnenberg <ntsonnenberg@gmail.com>
2024-11-27 18:34:25 -0800

    Added Button to home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import FileInput from "@/components/FileInput";
+import Btn01 from "@/components/kokonutui/btn-01";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-10 justify-center items-center h-screen">
       <div className="flex flex-col gap-4 items-center">
         <h1 className="font-bold text-4xl">Welcome to Quizzard</h1>
-        <h2 className="font-thin">Please upload a file to analyze</h2>
+        <h2 className="font-thin">Please upload a notes to analyze</h2>
       </div>
       <FileInput />
+      <Btn01 className="w-2/3">Create Quiz</Btn01>
     </div>
   );
 }
